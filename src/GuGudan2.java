53966079fe7be6c77cbd71d900ccc1ce0f69595d
al4122 <al4122@naver.com>
2024-05-18 23:44:14 +0900

    변경

--- src/GuGudan2.java
+++ src/GuGudan2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class GuGudan2 {
+public class GuGudan2 {
+//    public static void main(String[] args) {
+//        int[] result = new int[9];
+//        for(int i = 0; i < result.length;i++){
+//            result[i] = 2 * (i + 1);
+//            System.out.println(result[i]);
+//
+//        }
+//    }
+    public static int[] calculate(int times) {
+        int[] result = new int[9];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = times * (i + 1);
+        }
+        return result;
+    }
+    public static void print(int[] result){
+        for (int i = 0; i < result.length; i++){
+            System.out.println(result[i]);
+        }
+    }
+
+
 }
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class GugudanMain {
+public class GugudanMain {
+    public static void main(String[] args) {
+        for(int i = 2; i <10; i++){
+            int[] result = GuGudan2.calculate(i);
+            GuGudan2.print(result);
+        }
+
+    }
 }
